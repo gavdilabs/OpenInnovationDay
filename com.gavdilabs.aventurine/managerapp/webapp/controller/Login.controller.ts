ee2f6c6bc77e9def8fe7b8c6aceeb586698343fc
memgavdilabs <mem@gavdilabs.com>
2023-04-27 13:42:52 +0200

    manager app ui baseline

---
+++ com.gavdilabs.aventurine/managerapp/webapp/controller/Login.controller.ts
@@ -0,0 +1,22 @@
+import MessageBox from "sap/m/MessageBox";
+import BaseController from "./BaseController";
+import formatter from "../model/formatter";
+import Input from "sap/m/Input";
+import MessageToast from "sap/m/MessageToast";
+
+/**
+ * @namespace com.gavdilabs.ui5template.controller
+ */
+export default class Login extends BaseController {
+  private formatter = formatter;
+
+  public onLogin(): void {
+    const username = (this.byId("inp_userName") as Input).getValue();
+    if (username) {
+      MessageToast.show(`Welcome ${username}`);
+      //TODO: navigate to candidate screen
+    } else {
+      MessageBox.error("Enter your username");
+    }
+  }
+}
