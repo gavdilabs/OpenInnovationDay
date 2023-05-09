0334598657b40960da6c2995bcc336ba7378d041
Kasper_Amstrup <kaj@gavdilabs.com>
2023-05-09 09:20:38 +0200

    Created candidate overview page

---
+++ com.gavdilabs.malachite/app/webapp/controller/CandidateOverview.controller.ts
@@ -0,0 +1,14 @@
+import BaseController from "./BaseController";
+import Event from "sap/ui/base/Event";
+
+/**
+ * @namespace com.gavdilabs.ui5template.controller
+ */
+export default class CandidateOverview extends BaseController {
+
+	onListItemPress(oEvent:Event){
+		console.log("hi")
+	}
+
+
+}
